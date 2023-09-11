0e1b58d4abf709bbd769fa969d8cad3a4e8fd425
Jérémy <bruzacj@yahoo.fr>
2023-09-11 15:09:06 +0200

    feat(helmet): add helmet CSP default

--- myApp.js
+++ myApp.js
@@ -23,6 +23,14 @@ app.use(
 );
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self"],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
+    },
+  })
+);
 
 //
 
